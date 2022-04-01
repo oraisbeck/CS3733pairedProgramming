b7cafc838d47cf21d3a9ddadcd9a6ca80c8681b8
oliviaraisbeck <oeraisbeck@wpi.edu>
2022-04-01 15:13:37 -0400

    initial commit

---
+++ src/test/java/edu/wpi/teamname/DefaultTest.java
@@ -0,0 +1,15 @@
+/*-------------------------*/
+/* DO NOT DELETE THIS TEST */
+/*-------------------------*/
+
+package edu.wpi.teamname;
+
+import org.junit.Test;
+
+// import org.junit.jupiter.api.Test; (JUnit 5 equivalent)
+
+public class DefaultTest {
+
+  @Test
+  public void test() {}
+}
